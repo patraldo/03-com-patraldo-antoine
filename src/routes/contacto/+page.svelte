bb5a0241f7c19842d3ef7f32347fecf05dc0c069
Christophe R Patraldo <cheftech@patraldo.dev>
2023-06-26 14:08:37 -0600

    Initial commit

---
+++ src/routes/contacto/+page.svelte
@@ -0,0 +1,52 @@
+<div class='email'>
+<h1 style="font-size:7vw;"><a href = "mailto:antoine@patraldo.com">Email</a></h1>
+</div>
+<div class="masto">
+<h1 style="font-size:6vw;"><a rel="me" href="https://mastodonte.red/@antoine">Mastodonte Punto Red</a></h1>
+</div>
+
+
+<style>
+
+
+@import url('https://fonts.googleapis.com/css2?family=Marmelad&family=Pacifico&family=Special+Elite&family=Tangerine:wght@400;700&family=Ultra&display=swap');
+
+a, a:hover, a:active, a:visited, a:focus {
+color: black;
+    text-decoration:none;
+}
+
+.email {
+border: 10px dotted black;
+ background: goldenrod;
+ background-clip: padding-box;
+font-family: 'Special Elite';
+margin: 10px;
+padding: 10px;
+text-align: center;
+color: black;
+}
+
+.email a:hover {
+color: green;
+}
+
+.masto {
+ border: 10px dotted black;
+ background: lightblue;
+ background-clip: padding-box;
+font-family: 'Special Elite';
+margin: 10px;
+padding: 10px;
+text-align: center;
+color: blue;
+}
+
+.masto a:hover {
+color: teal;
+}
+
+
+</style>
+
+
